5a4ce2e63e8c53b83a4b7f8350b3ffa7eb66e370
HotHeadGranite <jgvvillocillo@gmail.com>
2025-02-10 03:20:44 +0000

    Basta may nasimulan

--- Customer.java
+++ Customer.java
@@ -1,4 +1,39 @@
-import java.util.Scanner;
 public class Customer {
+    public static void main(String[] args) {
+       public String name;
+       public String email;
+       public String address;
+       public int phone number;
+       public String customer type;
 
-}
+       public String getName() {
+        return name;
+       }
+
+       public void setName(String name) {
+        this.name = name;
+       }
+
+       public String getEmail() {
+        return email;
+       }
+
+       public void setEmail(String email) {
+        this.email = email;
+       }
+
+       public String getAddress() {
+        return address;
+       }
+
+       public void setAddress(String address) {
+        this.address = address;
+       }
+
+       public int getPhoneNumber() {
+        return phone number;
+       }
+
+       public void setPhoneNumber(int phone number) {
+        this.phone number = phone number;
+       }
