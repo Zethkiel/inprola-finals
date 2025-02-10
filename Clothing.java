bd716d1883ec36ae1394ec92a79d090806ddc315
Ari <ririariii15@gmail.com>
2025-02-10 02:25:41 +0000

    update

---
+++ Clothing.java
@@ -0,0 +1,38 @@
+public class Clothing {
+    public String color;
+    public Character size;
+    public Character gender;
+    public int quantity;
+
+
+   public Clothing() {
+   }
+   public void setColor(String var1) {
+      this.color = var1;
+   }
+   public String getColor(String var1) {
+      return this.color;
+   }
+
+   public void setSize(Character var1) {
+      this.size = var1;
+   }
+
+   public Character getSize(Character var1) {
+      return this.size;
+   }
+
+   public void setGender(Character var1) {
+        this.gender = var1;
+   }
+   public Character getGender(Character var1) {
+        return this.gender;
+   }
+
+   public void setQuantity(int var1) {
+        this.quantity = var1;
+   }
+   public int getQuantity(int var1){
+        return this.quantity;
+   }
+}
