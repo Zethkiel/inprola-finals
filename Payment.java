13b6a24ee739c35304274c8a7ca0ef7440b91490
clarken09 <trefire1998drop@gmail.com>
2025-02-17 01:47:59 +0000

    payment

--- Payment.java
+++ Payment.java
@@ -1,3 +1,36 @@
 public class Payment {
-    private 
+
+    private boolean paymentverification;
+    privateString paymentMethod;
+
+    public Payment() {
+        boolean paymentVerification;
+        String paymentMethod;
+    }
+
+    public Payment(bool pV, String pM) {
+        boolean paymentVerification = pV;
+        String paymentMethod = pM;
+    }
+
+    public Payment(String pM, bool pV) {
+        boolean paymentVerification = pV;
+        String paymentMethod = pM;
+    }
+
+    public void paymentMethod() {
+        if (paymentMethod.equalsIgnoreCase("credit")) {} 
+        else if (paymentMethod.equalsIgnoreCase("debit")) {} 
+        else if (paymentMethod.equalsIgnoreCase("check")) {}
+        else if (paymentMethod.equalsIgnoreCase("e-wallet")) {}
+        else if (paymentMethod.equalsIgnoreCase("cash")) {
+        } else {
+            System.out.println("The payment method is invalid. Please try again.");
+        }
+    }
+
+    public paymentVerify() {}
+
+    public isThePaymentBalanceSufficient() {}
+
 }
