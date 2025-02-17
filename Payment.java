9e03994d27e2959c79edb6fe997c98581af13554
clarken09 <trefire1998drop@gmail.com>
2025-02-17 02:46:54 +0000

    payment

--- Payment.java
+++ Payment.java
@@ -1,35 +1,46 @@
 public class Payment {
 
-    private boolean paymentverification;
-    privateString paymentMethod;
+    private String paymentMethod;
 
-    public Payment() {
-        boolean paymentVerification;
-        String paymentMethod;
-    }
-
-    public Payment(bool pV, String pM) {
-        boolean paymentVerification = pV;
-        String paymentMethod = pM;
-    }
-
-    public Payment(String pM, bool pV) {
-        boolean paymentVerification = pV;
-        String paymentMethod = pM;
+    public void setPaymentMethod(String pM) {
+        paymentMethod = pM;
     }
 
     public void paymentMethod() {
-        if (paymentMethod.equalsIgnoreCase("credit")) {} 
-        else if (paymentMethod.equalsIgnoreCase("debit")) {} 
-        else if (paymentMethod.equalsIgnoreCase("check")) {}
-        else if (paymentMethod.equalsIgnoreCase("e-wallet")) {}
-        else if (paymentMethod.equalsIgnoreCase("cash")) {
+        if (paymentMethod.equalsIgnoreCase("credit")) {
+        } else if (paymentMethod.equalsIgnoreCase("debit")) {
+        } else if (paymentMethod.equalsIgnoreCase("check")) {
+        } else if (paymentMethod.equalsIgnoreCase("e-wallet")) {
+        } else if (paymentMethod.equalsIgnoreCase("cash")) {
         } else {
             System.out.println("The payment method is invalid. Please try again.");
         }
     }
 
-    public paymentVerify() {}
+    public bool payment(double cost, double bal) {
+        if (paymentMethod.equalsIgnoreCase("credit")
+        || paymentMethod.equalsIgnoreCase("debit")
+        || paymentMethod.equalsIgnoreCase("check")
+        || paymentMethod.equalsIgnoreCase("ewallet")) {
+            if (cost < bal){
+                return true;
+            } else {
+                return false;
+            }
+        } else if (paymentMethod.equalsIgnoreCase(cash)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String paymentVerify() {
+        if (minusBalance() == true) {
+            return "The payment has been deducted to your balance./nThe payment is complete.";
+        } else {
+            return "You have insufficient balance for this purchase./nPlease make sure you have enough balance to proceed with the payment.";
+        }
+    }
 
     public isThePaymentBalanceSufficient() {}
 
