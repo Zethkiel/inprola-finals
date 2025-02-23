410be521b4686a97b762e333e059c010efd8ed4c
Ari <ririariii15@gmail.com>
2025-02-23 16:47:36 +0800

    Update Catalog: Inheritance, overriding

--- Catalog.java
+++ Catalog.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Catalog {
 
 
-    public void catalogType(int id) {
+    public void catalogId(int id) {
 
         if (id < 202409) {
             System.out.println("Priced according to THIS month's catalog (February 2025)");
@@ -13,6 +13,63 @@ public class Catalog {
         }
     }
 }
+class CloseOutItems extends Catalog{
+    public String section = "Close Out Items";
+    public void catalogId(int id) {
+
+        if (id < 202409) {
+            System.out.println("Priced according to THIS month's catalog (February 2025)");
+        }
+        else {
+            System.out.println("Priced according to catalog ON-HAND");
+
+        }
+    }
+    public void section() {
+        System.out.println("Welcome to " +section + " Section");
+        System.out.println("");
+
+    }
+}
+
+class MonthlySpecials extends Catalog{
+    public String section = "Monthly Specials";
+    public void catalogId(int id) {
+
+        if (id < 202409) {
+            System.out.println("Priced according to THIS month's catalog (February 2025)");
+        }
+        else {
+            System.out.println("Priced according to catalog ON-HAND");
+
+        }
+    }
+    public void section() {
+        System.out.println("Welcome to " +section + " Section");
+        System.out.println("Limited Edition: ");
+
+    }
+}
+
+class NormalPrices extends Catalog{
+    public String section = "Normal Prices";
+    public void catalogId(int id) {
+
+        if (id < 202409) {
+            System.out.println("Priced according to THIS month's catalog (February 2025)");
+        }
+        else {
+            System.out.println("Priced according to catalog ON-HAND");
+
+        }
+    }
+    public void section() {
+        System.out.println("Welcome to " +section + " Section");
+        System.out.println("New Collection: ");
+
+    }
+}
+
 class Main {
     public static void main(String[] args) {
 
@@ -21,6 +78,19 @@ class Main {
 
         System.out.println("Enter Catalog ID: ");
         int id = scanner.nextInt();
-        catalog.catalogType(id);
+        catalog.catalogId(id);
+        System.out.println(" ");
+
+        CloseOutItems closeOutItems = new CloseOutItems();
+        closeOutItems.section();
+        System.out.println(" ");
+
+        MonthlySpecials monthlySpecials = new MonthlySpecials();
+        monthlySpecials.section();
+        System.out.println(" ");
+
+        NormalPrices normalPrices = new NormalPrices();
+        normalPrices.section();
+        System.out.println(" ");
     }
 }
