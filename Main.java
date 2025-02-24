10f36a18770eaab9481db720ea383647487b1f8a
Ethan <142134295+Zoomerboi@users.noreply.github.com>
2025-02-24 10:11:17 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Clothing a = new Clothing();
+        a.clothing
+            
     }
 }
