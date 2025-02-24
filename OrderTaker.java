881062a7509cfabce64835f9793e362e026b1123
clarken09 <trefire1998drop@gmail.com>
2025-02-24 08:59:00 +0000

    ordertaker update

--- OrderTaker.java
+++ OrderTaker.java
@@ -3,8 +3,21 @@ public class OrderTaker {
     private Catalog cat;
 
     public void checkCustomerType() {
-        if (c.getCustomerType().IgnoreCaseEquals("subscriber")) {
-            
+        if (c.getCustomerType().equalsIgnoreCase("subscriber")) {
+            catalogOrder();
+        } else if (c.getCustomerType().equalsIgnoreCase("online")) {
+            onlineOrder();
         }
+    }
+
+    public void catalogOrder() {
+        //csr gives form
+        //pagsamahin ang order info dito(order id, etc)
+        //oec will send the form to itemOrder
+    }
+    public void onlineOrder() {
+        //shows the current catalog to user
+        //user buys
+        //send the 'cart' to itemorder
     }
 }
