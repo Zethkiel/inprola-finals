6839eac5a28577e079e66dea080b763b6de8ba46
Ethan <142134295+Zoomerboi@users.noreply.github.com>
2025-02-24 09:54:04 +0800

    Create Shipping.java
    under construction

---
+++ Shipping.java
@@ -0,0 +1,9 @@
+public class Shipping
+  if (x.equals("payment has been deducted to the balance./nThe payment is complete."){
+      System.out.println("The order has been shipped");
+  }
+  else if (x.equals("This purchase is through cash-on-delivery"){
+      System.out.println("The order has been shipped");
+  }
+  else { 
+      System.out.println("Transaction failed please try again");
